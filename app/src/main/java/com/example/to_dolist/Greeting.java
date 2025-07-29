965ebf69ea5a879012c26dc9c31c7fc326611248
Mohamed <mnafe44@gmail.com>
2025-07-29 22:38:43 +0300

    comment

--- app/src/main/java/com/example/to_dolist/Greeting.java
+++ app/src/main/java/com/example/to_dolist/Greeting.java
@@ -24,8 +24,5 @@ public class Greeting extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        new Handler().postDelayed(() -> {
-            finish();
-        }, 3000);
     }
 }
