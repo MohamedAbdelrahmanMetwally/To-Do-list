8ba1b941ff2fe9bfeac1aceb52d47c367551174e
Mohamed <mnafe44@gmail.com>
2025-08-06 06:26:30 +0300

    adding the task class

---
+++ app/src/main/java/com/example/to_dolist/core/database/Task.java
@@ -0,0 +1,21 @@
+package com.example.to_dolist.core.database;
+public class Task {
+    private String title;
+    private int id;
+    public Task(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
